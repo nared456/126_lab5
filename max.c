988e1250d26203f534236aaea372c4bd6e26fa53
nared456 <78350512+nared456@users.noreply.github.com>
2021-03-08 19:32:16 +0700

    6320503041

--- max.c
+++ max.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int N,M,i,j;
+    int N,M,i,j,k,l,sum=0,MAX=0;
     scanf("%d",&N);
     scanf("%d",&M);
     int A[N][N];
-    int B[M][M];
     for(i=0;i<N;i++)
     {
         for(j=0;j<N;j++)
@@ -13,5 +12,23 @@ int main()
             scanf("%d",&A[i][j]);
         }
     }
-    
+    for(i=0;i<N-M-1;i++)
+    {
+        for(j=0;i<N-M-1;i++)
+        {
+            for(k=i;k<M+i;k++)
+            {
+                for(l=j;l<M+j;l++)
+                {
+                    sum += A[k][l];
+                }
+            }
+            if(sum > MAX)
+            {
+                MAX = sum;
+            }
+        sum = 0;
+        }
+    }
+    printf("%d",MAX);
 }
--- stack.c
+++ stack.c
@@ -1,10 +1,52 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 int main()
 {
-    int n,i;
-    scanf("%d",&n);
-    char S[n];
-    scanf("%s",&S);
-    printf("%s",S);
+    int n, i, c = 0;
+    int num[100];
+    scanf("%d", &n);
+    char ps[n];
+    scanf("%s", &ps);
+    for (i = 0; i < n;)
+    {
+        if(ps[i]=='\0')
+            break;
+        if (ps[i] >= 48 && ps[i] <= 57)
+        {
+            if (ps[i + 4] >= 48 && ps[i + 4] <= 57 && ps[i + 3] >= 48 && ps[i + 3] <= 57 && ps[i + 2] >= 48 && ps[i + 2] <= 57 && ps[i + 1] >= 48 && ps[i + 1] <= 57)
+            {
+                num[c] = (10000 * (ps[i] - 48)) + (1000 * (ps[i + 1] - 48)) + (100 * (ps[i + 2] - 48)) + (10 * (ps[i + 3] - 48)) + (ps[i + 4] - 48);
+                i += 5;
+                c++;
+            }
+            else if (ps[i + 3] >= 48 && ps[i + 3] <= 57 && ps[i + 2] >= 48 && ps[i + 2] <= 57 && ps[i + 1] >= 48 && ps[i + 1] <= 57)
+            {
+                num[c] = (1000 * (ps[i] - 48)) + (100 * (ps[i + 1] - 48)) + (10 * (ps[i + 2] - 48)) + (ps[i + 3] - 48);
+                i += 4;
+                c++;
+            }
+            else if (ps[i + 2] >= 48 && ps[i + 2] <= 57 && ps[i + 1] >= 48 && ps[i + 1] <= 57)
+            {
+                num[c] = (100 * (ps[i] - 48)) + (10 * (ps[i + 1] - 48)) + (ps[i + 2] - 48);
+                i += 3;
+                c++;
+            }
+            else if (ps[i + 1] >= 48 && ps[i + 1] <= 57)
+            {
+                num[c] = (10 * (ps[i] - 48)) + (ps[i + 1] - 48);
+                i += 2;
+                c++;
+            }
+            else
+            {
+                num[c] = (ps[i] - 48);
+                i++;
+                c++;
+            }
+        }
+    }
+    for(i=0;i<c;i++);
+    {
+        printf("%d ",num[i]);
+    }
 }
